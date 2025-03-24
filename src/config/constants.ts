24e9c181e16f73f6a2bac04e0d16c0874b408c7a
Barış Arslan <arslan.bariss@outlook.com>
2025-03-24 17:13:02 +0300

    Update APP_URL in constants and adjust metadata image URL for consistency

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Next Store App";
 export const APP_DESCRIPTION = "Next Store App is a sample e-commerce application built with Next.js, Redux Toolkit, and Tailwind CSS.";
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "https://next-store-app.vercel.app";
+export const APP_URL = "https://next-store-app-ashen.vercel.app";
 
 export const routes = {
     home: () => '/',
